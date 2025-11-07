4cd323620e8f95b9f32b8f5ac99cf23c57b6c2de
Everpro <eyuri49@gmail.com>
2025-11-07 17:20:58 -0300

    changed the way to clean strange chars

--- src/treating/strangeChars.ts
+++ src/treating/strangeChars.ts
@@ -1,15 +1,10 @@
 import { conn } from "../db/conn";
 
 export function treatString(str: string): string {
-
-    const chars = [
-        '�', '職', 'ﾇ', 'ﾕ', '縊', 'ﾁ', 'ﾃ', 'ﾊ', 'ﾄ', 'ﾍ', 'ｰ', '兤', 'ﾓ', '鈬', '・', 'ﾔ', '輟', 'ｴ', 'ﾉ', 'ｺ', '匤', '軋', 'ﾂ', '黌', 'ﾌ', 'ﾚ', '侒', '﨎', '疝', '侔', '‡', 'Ƒ', '•', '€', '¡', '䰃', '偐', '!', '#', 'Ъ', 'Г', 'З', 'Й', 'Б', 'У', 'Н', 'Ф', 'К', 'Х', 'В', 'ƒ', 'Ґ', '・', '', '當', '疵', '騁', '疊', '匇', '疽', 'ﾑ', 'Т', 'И', 'Ј', 'Ѓ', '�', 'Є', '░', '┤', '', '', '‰', 'Щ', 'А', 'Ĺ', 'Ȋ', 'Ô', '├', 'Ë', 'Ü', 'ﾈ', 'ﾒ', 'Б', 'Ќ', 'Љ', 'Ђ', 'Љ', 'Ò', 'Μ', 'İ', '±', 'ｲ'
-    ];
-    str = str.replace('  ', ' ');
-    const regex = new RegExp(`[${chars.join('')}]`, 'g');
-    return str.replace(regex, '')
-        .replace(/[ÁÉÍÓÚáéíóúÂÊÔâêôÀàÜüÑñëïöüÿÄËÏÖÜŸåÅæÆœŒ]/g, '')
-        .replace(/[.\|,!@/\-\$%¨&*()+=§:;?¿�^~{}\´\`_º\[\]¬]/g, '');
+    return str
+    .replace(/ {2,}/g, ' ')               // collapse double spaces
+    .replace(/[^A-Za-z0-9À-ÿ\s]/g, '')    // keep: letters, numbers, accents, spaces
+    .trim();
 }
 
 export function treatGtin(str: string): string {
